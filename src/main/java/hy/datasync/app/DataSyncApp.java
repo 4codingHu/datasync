4f52fa263b8bb3a9c910de9de325f2d261b845f4
huyang <huyang2984@tops001.com>
2016-08-07 11:09:29 +0800

    添加canal maven依赖等

--- src/main/java/hy/datasync/app/DataSyncApp.java
+++ src/main/java/hy/datasync/app/DataSyncApp.java
@@ -3,7 +3,9 @@ package hy.datasync.app;
 public class DataSyncApp {
 	
 	public static void main(String... args) {
+		System.out.println("DataSyncApp is started!");
 		
+		System.out.println("DataSyncApp is finished!");
 	}
 	
 	
